5a9236ed46400656f2b4095fe3135c50ba4bb29c
Venkatesh Rajendran <rcvenky@yahoo.com>
2021-05-22 10:09:35 +0530

    Create Swapping Nodes in a Linked List.cpp

---
+++ Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,59 @@
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+  
+  
+  class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+         int r=0;
+        ListNode* curr=head,*left=head,*right=head;
+        while(curr!=NULL)
+        {
+            curr=curr->next;
+            r++;
+        }
+        for(int i=1;i<k;i++)
+        {
+            left=left->next;
+        }
+        r=r-k;
+        for(int i=1;i<=r;i++)
+        {
+            right=right->next;
+        }
+        swap(left->val,right->val);
+        return head;
+    }
+};
